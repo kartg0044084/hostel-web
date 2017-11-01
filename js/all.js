814c21b9a01b0976948d6f1bba91042080b756e4
kartg0044084 <32888970+kartg0044084@users.noreply.github.com>
2017-11-01 09:36:07 +0800

    0.0.9
    admin 部分:
    最新消息完成
    套裝行程完成
    訂房分類完成
    房型完成
    新增:
    使用者權限
    正常登入登出查詢
    loding facebook特效
    
    前台:
    index 建構中

--- js/all.js
+++ js/all.js
@@ -1,8 +1,9 @@
 jQuery(document).ready(function($) {
-    $("#loading").show();//显示loading
-    setTimeout("$('#loading').hide();", 2500);//隐藏loading
+      $(".container").show();//显示loading
+      setTimeout("$('.container').hide();", 2500);//隐藏loading
 
-    $(".icon-menu").click(function(){
-  $("#navbar").slideToggle();
-});
+      $('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('#overlay').toggleClass('open');
+  });
 });
